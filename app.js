594094554ceb8ae0d632e05437de21a0aa2d2ef3
Ethan Lloyd <20076846@H2WHK18TQ6NW.tld>
2022-08-26 08:09:59 -0500

    Got sentences to generate, but not in the correct way; fix on monday

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 function getData() {
     var place = document.getElementById("place").value;
-    var name = document.getElementById("name").value;
+    var person = document.getElementById("person").value;
     var verb = document.getElementById("verb").value;
     var adj = document.getElementById("adjective").value;
-    var adverb = document.getElementById("adverb").value;
-    return [place, name, verb, adj, adverb]
-    console.log([place, name, verb, adj, adverb]);
+    var adverb = document.getElementById("adverb").value;    
+
+
+    document.getElementById("sentence").innerHTML = sentences(verb, adverb, person, ajd, place);
 }
+
+function sentences(place, person, verb, adj, adverb) {
+    sentenceArray = [`Today I had to ${verb} ${adverb}. But right in the middle of it, ${person} came with this ${adj} book and wanted to go to ${place}.`, `${person} was in ${place} trying to ${verb}. However, ${person} left a ${adj} shirt, while crying ${adverb}.`, `It's been a long time since ${person} decided to ${verb} in ${place}. What some people said was that ${person} screamed "This straberry is ${adj}!!" We thought ${person} went crazy!`]
+
+    ranN = Math.floor(Math.random() * sentenceArray.length)
+return sentenceArray[ranN]
+}
+
